f5686fb7602abb4179233bc98a75ccd16aea0355
Furkan Semizoğlu <furkan5590@hotmail.com>
2025-02-10 12:11:09 +0300

    added prisma

---
+++ prisma/migrations/20250210085507_product_trial/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "ProductTrial" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductTrial_pkey" PRIMARY KEY ("id")
+);
